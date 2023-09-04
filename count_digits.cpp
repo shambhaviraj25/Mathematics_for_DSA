432118974fd93c156fabdb810f1e5ad21fa57372
shambhaviraj25 <kashyapshambhavi1410@gmail.com>
2023-09-04 20:40:55 +0530

    count_of_digit_in_a_number

---
+++ count_digits.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int countDigits(int x)
+{
+   int res=0;
+   while(x>0)
+   {
+     x=x/10;
+     res++;
+   }
+   return res;
+}
+int main(){
+    int number=7894;
+    cout<<countDigits(number);
+    return 0;
+}
